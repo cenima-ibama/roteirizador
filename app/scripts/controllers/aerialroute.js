acc591f4dd9632d4df875ea9d31c7a0e5b8cef06
Wille Marcel <wille@wille.blog.br>
2015-02-27 18:02:53 -0300

    add autocomplete and sendRoute function. improve airport selection.

--- app/scripts/controllers/aerialroute.js
+++ app/scripts/controllers/aerialroute.js
@@ -23,32 +23,72 @@ angular.module('routesClientApp')
           attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
       }).addTo(map);
 
-      $scope.setOrigin = function(airportId) {
+      $scope.originLayer = L.layerGroup().addTo(map);
+      $scope.destinationLayer = L.layerGroup().addTo(map);
+
+      $scope.setOrigin = function(airportId, name, latLng) {
         $scope.origin = airportId;
+        $('#origin').val(name);
+        $scope.originLayer.clearLayers();
+        L.marker(latLng).addTo($scope.originLayer);
       };
 
-      $scope.setDestination = function(airportId) {
+      $scope.setDestination = function(airportId, name, latLng) {
         $scope.destination = airportId;
+        $('#destination').val(name);
+        $scope.destinationLayer.clearLayers();
+        L.marker(latLng).addTo($scope.destinationLayer);
       };
 
       $scope.resetRoute = function() {
         $scope.origin = $scope.destination = '';
+        $scope.originLayer.clearLayers();
+        $scope.destinationLayer.clearLayers();
+        $('#origin').val('');
+        $('#destination').val('');
+      };
+
+      $scope.sendRoute = function() {
+        if ($scope.origin  && $scope.destination) {
+          RestApi.save({type: 'aerial-routes'},
+            {
+              'auth_code': $routeParams.authCode,
+              'states': $routeParams.states.split(','),
+              'origin': $scope.origin,
+              'destination': $scope.destination
+            },
+            function success() {
+              alert('success');
+            },
+            function error() {
+              $scope.error = true;
+              $scope.message = 'A rota não está dentro dos estados permitidos. ' +
+                'Trace novamente.';
+              $scope.class = 'danger';
+            }
+          )
+        }
+        else {
+          $('#popoverNoRoute').popover('show');
+        }
       };
 
       RestApi.query({type:'aerial-routes', authCode:$routeParams.authCode},
         function success(data) {
-          // origin airport
+          // add origin airport to map
           L.circleMarker(data.geometry.coordinates[0].reverse(),
             {weight: 1, opacity: 0.8, color: 'green'}
           ).setRadius(7)
           .bindPopup('Aeroporto de Origem')
           .addTo(map).openPopup();
-          // destination airport
+
+          // add destination airport to map
           L.circleMarker(data.geometry.coordinates[1].reverse(),
             {weight: 1, opacity: 0.8, color: 'red'}
           ).setRadius(7)
           .bindPopup('Aeroporto de Destino')
           .addTo(map);
+
           $scope.routeExists = true;
           $scope.message = 'Rota com código de autorização: ' + $routeParams.authCode;
           $scope.class = 'success';
@@ -61,10 +101,36 @@ angular.module('routesClientApp')
                 var popupScope = $scope.$new();
                 popupScope.name = feature.properties.name;
                 popupScope.id = feature.id;
+                popupScope.latLng = feature.geometry.coordinates.reverse();
                 return L.circleMarker(latlng, {weight: 1, opacity: 0.8})
                   .setRadius(5)
                   .bindPopup($compile(angular.element(popupTpl))(popupScope)[0]);
               },
+            });
+
+            $scope.airportList = data.features.map(
+              function(i) {
+                return {value: i.id,
+                  label: i.properties.name,
+                  coordinates: i.geometry.coordinates
+                  };
+              }
+            );
+            $('#origin').autocomplete({
+              minLength: 4,
+              source: $scope.airportList,
+               select: function(event, ui) {
+                 $scope.setOrigin(ui.item.value, ui.item.label, ui.item.coordinates);
+                 return false;
+               }
+            });
+            $('#destination').autocomplete({
+              minLength: 4,
+              source: $scope.airportList,
+               select: function(event, ui) {
+                 $scope.setDestination(ui.item.value, ui.item.label, ui.item.coordinates);
+                 return false;
+               }
             });
             $scope.airports.addTo(map);
           });
